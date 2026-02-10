c384f1d398dcb70a27b0d00f9a1bdd32ad3c000c
Yisehak Wondwossen <144843233+Yisakwondwosen@users.noreply.github.com>
2026-02-10 05:10:16 -0800

    Hide sender email and password
    Obscured sensitive email credentials in dta.py.

--- dta.py
+++ dta.py
@@ -10,53 +10,20 @@ from email.mime.multipart import MIMEMultipart
 # --- AUTHENTICATION ---
 SMTP_SERVER = "smtp.zoho.com"
 SMTP_PORT = 465
-SENDER_EMAIL = "yisehak@knotifyapp.com" 
-SENDER_PASSWORD = "Imagine4488$" 
+SENDER_EMAIL = "********" 
+SENDER_PASSWORD = "***********" 
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # --- DATA MANAGEMENT ---
 # These are everyone who was successful in your last two log windows
 already_sent = [
-    "hazaba19@gmail.com", "abdirisak.gelle@gmail.com", "abdullahi@mtii.edu.so", 
-    "abunajk@gmail.com", "abelngengele@gmail.com", "gaellrop@gmail.com",
-    "adelaidejachi33@gmail.com", "advaxe.mucatcha@gmail.com", "ches7alex@gmail.com",
-    "yitzakwondwosen@gmail.com", "megdad.elfaki@gmail.com", "alphonsebyembao8@gmail.com",
-    "mnuwayo@gmail.com", "ansasirajanet19@gmail.com", "a.said@mediaconvergency.co.tz",
-    "nukuriardinemartine@gmail.com", "assilaabdallah@gmail.com", "mulukluka22@gmail.com",
-    "joycegerold0@gmail.com", "atukundamartha911@gmail.com", "violadut20@gmail.com",
-    "bamlakfellekecc@gmail.com", "barnabaosman1916@gmail.com", "bekagetnet@gmail.com",
-    "betelhemghere21@gmail.com", "btabbynamwone@gmail.com", "aterchan54@gmail.com",
-    "chemustojoel@gmail.com", "jeankarani@gmail.com", "collinsyogoh@gmail.com",
-    "ellairadukunda06@gmail.com", "bida@bidathomas.me", "jimmyezra15@gmail.com",
-    "erickmnyali14@gmail.com", "esayasyohannes12@gmail.com", "esta@medpack.co.tz",
-    "fabrisioyibrah@gmail.com", "fevenalemu2008@gmail.com", "gloriahtirot@gmail.com",
-    "21gracedavid@gmail.com", "graceknamatende@gmail.com", "isaacmuhumuza881@gmail.com",
-    "israelasfaw7@gmail.com", "jamesmayenmachiek@gmail.com", "jescamagashi101@gmail.com",
-    "umujoie2002@gmail.com", "fallkadidja20@gmail.com", "elishakakanyero5@gmail.com",
-    "kalohissen@gmail.com", "keitesiaisha@gmail.com", "l9chambiri@gmail.com",
-    "lucytomeka@gmail.com", "lnnjoroge@dtbafrica.com", "bereketlulia942@gmail.com",
-    "kosgeylynet@gmail.com", "meklitmnot@gmail.com", "magrethdmkumbo@gmail.com", 
-    "peacemasika8@gmail.com", "mrezy45@gmail.com", "priscamukelenga006@gmail.com", 
-    "joe604590@gmail.com", "ishimwenancysabrina1@gmail.com", "nancygakii25@gmail.com",
-    "mahamedkadir12@gmail.com", "empowerrisehub@gmail.com", "nahomkilng@gmail.com",
-    "trudyvanitah@gmail.com", "nakalemashakirah6@gmail.com", "natnaeldechasa78@gmail.com",
-    "ndayivenuste11@gmail.com", "zzindarutz@gmail.com", "ratifaniringiyimana79@gmail.com",
-    "nibitangaruth0@gmail.com", "njukiann4@gmail.com", "dushakenoella@gmail.com",
-    "nyuonlual7@gmail.com", "pauln5816@gmail.com", "rachealmonic5@gmail.com",
-    "rodney.omondi254@gmail.com", "yakarimu.2024@gmail.com", "salimseleman722@gmail.com",
-    "kukusaeed1996@gmail.com", "selamsun7@gmail.com", "africa.com.et@gmail.com",
-    "mukalushamid@gmail.com", "sijin.tuony@nobilisplus.ch", "simon.ntugi@gmail.com",
-    "habteabsoliana@gmail.com", "iuwadukunze@gmail.com", "ibrahim@zati.africa",
-    "wayneotemahegesa@gmail.com", "nebiyugirma19@gmail.com", "timbambala6@gmail.com"
+    
 ]
 
 # Remaining to be sent:
 recipients = [
-    ("Tinbite", "dawittinbite22@gmail.com"),
-    ("Xaviour", "alukuxaviour@gmail.com"),
-    ("Zainab", "zeynoba00@gmail.com"),
-    ("Zamzam", "zammc05@gmail.com")
+    
 ]
 
 # --- IMAGE ENCODING ---
